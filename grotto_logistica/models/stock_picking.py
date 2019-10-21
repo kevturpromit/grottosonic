a096404ef1877e26f18b15024e58b9197dc9e691
Jonathan Guacaran <dsa.software@hotmail.com>
2019-10-21 14:42:14 -0430

    Quitar validacion de factura

--- grotto_logistica/models/stock_picking.py
+++ grotto_logistica/models/stock_picking.py
@@ -5,6 +5,7 @@ from odoo import models, fields, api, _
 class StockPicking(models.Model):
     _inherit = 'stock.picking' 
 
+    """
     @api.multi
     def create_invoice(self):
         super(StockPicking,self).create_invoice()
@@ -12,3 +13,4 @@ class StockPicking(models.Model):
         for picking_id in picking_ids.filtered(lambda l:l.state == 'done'):
             for invoice_id in picking_id.invoice_ids:
                 invoice_id.action_invoice_open()
+                """
