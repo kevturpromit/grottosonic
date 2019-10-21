38b48ed7858313f27bbdf22759692eed433faa57
Jonathan Guacaran <dsa.software@hotmail.com>
2019-10-21 14:28:48 -0430

    Validar factura al crear

--- grotto_logistica/models/stock_picking.py
+++ grotto_logistica/models/stock_picking.py
@@ -2,6 +2,13 @@
 
 from odoo import models, fields, api, _
 
-class dsa_modulo(models.Model):
+class StockPicking(models.Model):
     _inherit = 'stock.picking' 
 
+    @api.multi
+    def create_invoice(self):
+        super(StockPicking,self).create_invoice()
+        picking_ids = self.env['stock.picking'].browse(self.env.context.get('active_ids'))
+        for picking_id in picking_ids.filtered(lambda l:l.state == 'done'):
+            for invoice_id in picking_id.invoice_ids:
+                invoice_id.action_invoice_open()
