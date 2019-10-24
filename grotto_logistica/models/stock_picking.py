d4b6ede083a08de197ac81e0448983458b21b5bd
Jonathan Guacaran <dsa.software@hotmail.com>
2019-10-23 20:53:55 -0430

    Reporte Resmen de Ordenes de Entrega

--- grotto_logistica/models/stock_picking.py
+++ grotto_logistica/models/stock_picking.py
@@ -5,6 +5,40 @@ from odoo import models, fields, api, _
 class StockPicking(models.Model):
     _inherit = 'stock.picking' 
     
+    @api.model
+    def get_data(self, picking_ids=None):
+        
+        picking_ids = self._context('active_ids',picking_ids)
+
+        sheet_search = self.env['stock.move'].search(
+            [('picking_id', 'in', picking_ids)])
+        
+        _logger.info('Pickings :%s'%picking_ids)
+        _logger.info('sheet_search :%s'%sheet_search)
+
+
+        lst = []
+        product = []
+        for sheet in sheet_search:
+            _logger.info('Producto:%s'%sheet.product_id.name)
+            product[sheet.product_id.id] = {'product_id':sheet.product_id.id,
+                                            'product_name':sheet.product_id.name,
+                                            'cantidad':0.00}
+        for sheet in sheet_search:
+            dic = {}
+            _logger.info('Cantidad:(%s)-%s'%(sheet.product_uom_qty,sheet.product_id.name))
+            product[sheet.product_id.id]['cantidad'] += sheet.product_uom_qty
+
+        for x in product:
+            dic = {
+                'product_name': x['product_name'],
+                'cantidad': x['cantidad']
+                }
+            _logger.info('x:%s'%x)
+            lst.append(dic)
+        return [{'total': len(lst),
+                 'line': lst}]
+        
     
 class WizardStockPickingInvoice(models.TransientModel):
     _inherit = 'wizard.stock.picking.invoice'    
--- grotto_logistica/reports/stock_picking_resumen_report.py
+++ grotto_logistica/reports/stock_picking_resumen_report.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from datetime import datetime, timedelta
+from odoo import api, models, _
+from odoo.exceptions import UserError
 
-from odoo import models, fields, api
-from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT as DATETIME_FORMAT
+import logging
 
+_logger = logging.getLogger(__name__)
 
 class StockPickingResumenReport(models.AbstractModel):
     """Abstract Model for report template.
@@ -12,24 +14,71 @@ class StockPickingResumenReport(models.AbstractModel):
     """
 
     _name = 'report.grotto_logistica.stock_picking_despacho'
-
-    @api.model
-    def get_report_values(self, docids, data=None):
-
-        docs = []
         
-        picking_ids = self.env['stock.picking'].search([('id','in',self._context('active_ids'))], order='name asc')
-        for picking in pikcing_ids:            
-            docs.append({
-                'name': pikcing.name,
-                'date': picking.date,
-                'partner_id': picking.partner_id.name,
-            })
+    def get_data(self, data, pkids):
 
-        return {
-            'doc_ids': data['ids'],
-            'doc_model': data['model'],
-            'date_start': date_start,
-            'date_end': date_end,
+        sheet_search = self.env['stock.move'].search(
+            [('picking_id', 'in', pkids)])
+        
+        _logger.info('Pickings :%s'%pkids)
+        _logger.info('sheet_search :%s'%sheet_search)
+
+
+        lst = []
+        product = {}
+        for sheet in sheet_search:
+            if sheet.product_id:
+                _logger.info('Producto:%s'%sheet.product_id.name)
+                product[sheet.product_id.id] = {'product_id':sheet.product_id.id,
+                                                'product_name':sheet.product_id.name,
+                                                'cantidad':0.00}
+        for sheet in sheet_search:
+            if sheet.product_id:
+                _logger.info('Cantidad:(%s)-%s'%(sheet.product_uom_qty,sheet.product_id.name))
+                product[sheet.product_id.id]['cantidad'] += sheet.product_uom_qty
+
+        for x in product:
+            _logger.info('X: %s'%x)
+            dic = {
+                'product_name': product[x]['product_name'],
+                'cantidad': product[x]['cantidad']
+                }
+            _logger.info('x:%s'%product[x])
+            lst.append(dic)
+        return [{'total': len(lst),
+                 'line': lst}]
+        
+    """
+    @api.model
+    def render_html(self, docids, data=None):
+        report_obj = self.env['report']
+        report = report_obj._get_report_from_name('grotto_logistica.stock_picking_despacho')
+
+        custom_data = self.get_data()
+
+        docargs = {
+            'doc_ids': docids,
+            'doc_model': report.model,
+            'docs': self,
+            'get_data': custom_data,
+        }   
+        return report_obj.render('grotto_logistica.stock_picking_despacho', docargs)                
+       """ 
+    
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        model = self.env.context.get('active_model')
+        active_ids = self.env.context.get('active_ids') or False
+        docs = self.env['stock.picking'].browse(docids)
+        _logger.info('model:%s'%model)
+        _logger.info('active_ids:%s'%active_ids)
+        _logger.info('docids:%s'%docids)
+        _logger.info('docs:%s'%docs)
+        docargs = {
+            'doc_ids': self.ids,
+            'doc_model': model,
             'docs': docs,
+            'get_data': self.get_data(data, docids),
         }
+        return docargs
+        
